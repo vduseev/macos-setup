4cb71aa940ab23e3a110e33d3db60db11b8fae32
vduseev <vagiz.d@gmail.com>
2018-08-11 13:19:02 +0300

    Added admin and term verifications, and sudo access

--- install.sh
+++ install.sh
@@ -1,5 +1,23 @@
 #!/bin/bash
 
+# Verify that user belongs to the admin group
+if ! id -Gn $(whoami) | grep -q -w admin; then
+  echo 'ERROR: This script must be ran by the admin user.';
+  exit 1
+fi 
+
+# Verify script is ran from the terminal
+if ! [ -t 1 ]; then
+  echo 'ERROR: The script is tailored to be ran from the terminal.';
+  exit 1
+fi
+
+# Request sudo access from user
+# Here, $0 expands to the name of the script, and $@ expands to
+# the list of parameters passed to the script
+sudo "$0" "$@"
+
+
 # Change work directory to the user's home
 cd ~
 
