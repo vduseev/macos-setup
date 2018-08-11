a8c6c86ed65acd3ac6c9c22f7c7747dbb4bb27d2
vduseev <vagiz.d@gmail.com>
2018-08-11 13:23:30 +0300

    Added info message for sudo request

--- install.sh
+++ install.sh
@@ -2,19 +2,21 @@
 
 # Verify that user belongs to the admin group
 if ! id -Gn $(whoami) | grep -q -w admin; then
-  echo 'ERROR: This script must be ran by the admin user.';
+  echo 'ERROR: This script must be ran by the admin user.'
   exit 1
 fi 
 
 # Verify script is ran from the terminal
 if ! [ -t 1 ]; then
-  echo 'ERROR: The script is tailored to be ran from the terminal.';
+  echo 'ERROR: The script is tailored to be ran from the terminal.'
   exit 1
 fi
 
 # Request sudo access from user
 # Here, $0 expands to the name of the script, and $@ expands to
 # the list of parameters passed to the script
+echo 'INFO: Please provide sudo password...'
+echo "$0"
 sudo "$0" "$@"
 
 
