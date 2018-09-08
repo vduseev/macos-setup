151618453aba45f9465d25212359b917e15a90c9
vduseev <vagiz.d@gmail.com>
2018-09-08 19:18:18 +0300

    Added comments and repo downloads

--- install.sh
+++ install.sh
@@ -1,4 +1,20 @@
-#!/bin/bash
+#!/usr/bin/env bash
+
+# bootstrap.sh
+# This is a bootstrap script intended to be downloaded and launched
+# in one step.
+# It downloads required files and starts the installation script.
+#
+# Strategy:
+# - Run basic verifications
+#   - Are running as an admin user
+#   - Are we running from a terminal
+# - Git clone the setup repository and bash enhancement framework
+#   - Just clone the setup repository
+#   - Clone bash-oo-framework
+#   - Copy `lib` folder to the setup repository
+#   - Remove cloned bash-oo-framework
+# - Launch install script
 
 # Verify that user belongs to the admin group
 if ! id -Gn $(whoami) | grep -q -w admin; then
@@ -12,9 +28,13 @@ if ! [ -t 1 ]; then
   exit 1
 fi
 
-# Change work directory to the user's home
-cd ~
+# Start with cloning the repositories
+git clone https://github.com/vduseev/macos-setup.git $TMPDIR/macos-setup
+git clone https://github.com/niieani/bash-oo-framework.git $TMPDIR/bash-oo-framework
 
-# Start with cloning the repository
-git clone https://github.com/vduseev/macos-setup.git ~/Source/macos-setup
-chown -R $(whoami) ~/Source
+# Copy the framework files from repo and erase it
+cp $TMPDIR/bash-oo-framework/lib $TMPDIR/macos-setup
+rm -rf $TMPDIR/bash-oo-framework
+
+# Launch installation script
+$TMPDIR/macos-setup/install.sh
