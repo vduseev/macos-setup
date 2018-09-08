32de9ee802ed23c1d5c7381006495287aee54812
vduseev <vagiz.d@gmail.com>
2018-09-08 19:41:07 +0300

    Added install.sh draft with logging

---
+++ install.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+# install.sh
+# This is a main installation script.
+# It is assumed that bash-oo is already available at the /lib/ directory
+
+import util/log
+import util/tryCatch
+import util/exception
+
+# Declare logging namespace
+namespace Installation
+
+Log "Installation script is running!"
+
+exit 4
