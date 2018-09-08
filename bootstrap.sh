3a1da6356cece61eb588f472493c2bf4375d0808
vduseev <vagiz.d@gmail.com>
2018-09-08 22:28:35 +0300

    Added basic exception and logging support

--- bootstrap.sh
+++ bootstrap.sh
@@ -16,42 +16,56 @@
 #   - Remove cloned bash-oo-framework
 # - Launch install script
 
-# Verify that user belongs to the admin group
-if ! id -Gn $(whoami) | grep -q -w admin; then
-  echo 'ERROR: This script must be ran by the admin user.'
-  exit 1
-fi 
-
-# Verify script is ran from the terminal
-if ! [ -t 1 ]; then
-  echo 'ERROR: The script is tailored to be ran from the terminal.'
-  exit 1
-fi
-
 # Start with cloning the repositories
+echo -e "\033[1;32m[NOTICE  ] Cloning macos-setup to the temporary location...\033[0m"
 git clone https://github.com/vduseev/macos-setup.git "${TMPDIR}macos-setup"
-git clone https://github.com/niieani/bash-oo-framework.git "${TMPDIR}bash-oo-framework"
+#git clone https://github.com/niieani/bash-oo-framework.git "${TMPDIR}bash-oo-framework"
+
+(
+  # Activate bash logging library
+  source "${TMPDIR}macos-setup/lib/b-log.sh"
+  LOG_LEVEL_ALL
+
+  NOTICE "Running verification checks..."
+
+  # Verify that user belongs to the admin group
+  if ! id -Gn $(whoami) | grep -q -w admin; then
+    FATAL "ERROR: This script must be ran by the admin user."
+    exit 1
+  fi 
+
+  # Verify script is ran from the terminal
+  if ! [ -t 1 ]; then
+    FATAL "ERROR: The script is tailored to be ran from the terminal."
+    exit 1
+  fi
+)
 
 # Copy the framework files from repo and erase it
-cp -R "${TMPDIR}bash-oo-framework/lib" "${TMPDIR}macos-setup"
-rm -rf "${TMPDIR}bash-oo-framework"
+#cp -R "${TMPDIR}bash-oo-framework/lib" "${TMPDIR}macos-setup"
+#rm -rf "${TMPDIR}bash-oo-framework"
 
 # Activate bash-oo-framework
-source "${TMPDIR}macos-setup/lib/oo-bootstrap.sh"
+#source "${TMPDIR}macos-setup/lib/oo-bootstrap.sh"
 
 # Enable try-catch and exception printing functionality
-import util/tryCatch
-import util/exception
+#import util/tryCatch
+#import util/exception
 
-try {
-  # Launch installation script
+#try {
+  ## Launch installation script
+  #exec "${TMPDIR}macos-setup/install.sh"
+#} catch {
+  #echo "Caught Exception:$(UI.Color.Red) $__BACKTRACE_COMMAND__ $(UI.Color.Default)"
+  #echo "File: $__BACKTRACE_SOURCE__, Line: $__BACKTRACE_LINE__"
+
+  #Exception::PrintException "${__EXCEPTION__[@]}"
+#}
+
+(
   exec "${TMPDIR}macos-setup/install.sh"
-} catch {
-  echo "Caught Exception:$(UI.Color.Red) $__BACKTRACE_COMMAND__ $(UI.Color.Default)"
-  echo "File: $__BACKTRACE_SOURCE__, Line: $__BACKTRACE_LINE__"
-
-  Exception::PrintException "${__EXCEPTION__[@]}"
-}
+)
 
 # Perform final cleanup
+echo -e "\033[1;32m[NOTICE  ] Removing macos-setup from the temporary location...\033[0m"
 rm -rf "${TMPDIR}macos-setup"
--- install.sh
+++ install.sh
@@ -2,15 +2,8 @@
 
 # install.sh
 # This is a main installation script.
-# It is assumed that bash-oo is already available at the /lib/ directory
 
-import util/log
-import util/tryCatch
-import util/exception
+NOTICE "Installing pip..."
+#sudo easy_install pip
 
-# Declare logging namespace
-namespace Installation
 
-Log "Installation script is running!"
-
-exit 4
