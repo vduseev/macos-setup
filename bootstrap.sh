393e5516676b796f1adc94444bf16185edfca23f
vduseev <vagiz.d@gmail.com>
2018-10-28 19:22:02 +0300

    Update iteration for bootstrap file

--- bootstrap.sh
+++ bootstrap.sh
@@ -9,63 +9,161 @@
 # - Run basic verifications
 #   - Are running as an admin user
 #   - Are we running from a terminal
-# - Git clone the setup repository and bash enhancement framework
-#   - Just clone the setup repository
-#   - Clone bash-oo-framework
-#   - Copy `lib` folder to the setup repository
-#   - Remove cloned bash-oo-framework
 # - Launch install script
 
-# Start with cloning the repositories
-echo -e "\033[1;32m[NOTICE  ] Cloning macos-setup to the temporary location...\033[0m"
-git clone https://github.com/vduseev/macos-setup.git "${TMPDIR}macos-setup"
-#git clone https://github.com/niieani/bash-oo-framework.git "${TMPDIR}bash-oo-framework"
+#################################################
+#         LOGGING METHODS AND CONSTANTS         #
+#################################################
 
-(
-  # Activate bash logging library
-  source "${TMPDIR}macos-setup/lib/b-log.sh"
-  LOG_LEVEL_ALL
+RED="\033[0:31m"
+GREEN="\033[1:32m"
+NC="\033[0m"  # No Color
 
-  NOTICE "Running verification checks..."
+info() {
+  echo -e "[INFO  ] $1"
+}
+notice() {
+  echo -e "${GREEN}[NOTICE]${NC} $1"
+}
+error() {
+  echo -e "${RED}[ERROR ]${NC} $1"
+}
+
+##################################################
+#                    MAIN LOGIC                  #
+##################################################
+
+main() {
+  # Development mode: dev, prod
+  local __mode=$1
+
+   # Perform basic verifications
+  if ! run_checks; then exit 1; fi
+
+  # Clone main repository
+  if ! clone_repository_to_tmp_dir $__mode; then exit 1; fi
+
+  # Install required software
+  if ! install; then exit 1; fi
+
+  # Perform final cleanup
+  if ! cleanup_tmp_dir; then exit 1; fi
+}
+
+#################################################
+#                 HELPER METHODS                #
+#################################################
+
+# Run verification checks:
+# 1. Verify that script is ran by admin user
+# 2. Verify that script has been started from the terminal
+#
+run_checks() {
+  notice "Running verification checks ..."
 
   # Verify that user belongs to the admin group
   if ! id -Gn $(whoami) | grep -q -w admin; then
-    FATAL "ERROR: This script must be ran by the admin user."
-    exit 1
+    error "This script must be ran by the admin user."
+    return 1
   fi 
 
   # Verify script is ran from the terminal
   if ! [ -t 1 ]; then
-    FATAL "ERROR: The script is tailored to be ran from the terminal."
-    exit 1
+    error "The script is tailored to be ran from the terminal."
+    return 1 
   fi
-)
 
-# Copy the framework files from repo and erase it
-#cp -R "${TMPDIR}bash-oo-framework/lib" "${TMPDIR}macos-setup"
-#rm -rf "${TMPDIR}bash-oo-framework"
+  return 0
+}
 
-# Activate bash-oo-framework
-#source "${TMPDIR}macos-setup/lib/oo-bootstrap.sh"
+# Clone macos-setup repository to the tmp directory
+# to proceed further with the installation.
+# If script is running in "dev" mode, then instead
+# of clonning just copy files from script directory
+# to the tmp directory.
+#
+# Arguments:
+#   mode - "dev" or "prod"
+#
+# Returns:
+#   0 if success
+#   1 if failure
+#
+clone_repository_to_tmp_dir() {
+  # If mode is "dev", then don't clone the repo
+  local __mode=$1
 
-# Enable try-catch and exception printing functionality
-#import util/tryCatch
-#import util/exception
+  # Returns script's directory
+  local __script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+  WORK_DIR="${TMPDIR}macos-setup"
 
-#try {
-  ## Launch installation script
-  #exec "${TMPDIR}macos-setup/install.sh"
-#} catch {
-  #echo "Caught Exception:$(UI.Color.Red) $__BACKTRACE_COMMAND__ $(UI.Color.Default)"
-  #echo "File: $__BACKTRACE_SOURCE__, Line: $__BACKTRACE_LINE__"
+  # Start with cloning the repositories
+  notice "Cloning macos-setup to the temporary location ..."
+  notice "${WORK_DIR}"
 
-  #Exception::PrintException "${__EXCEPTION__[@]}"
-#}
+  # Use local directory instead of the remote one 
+  # if mode is set to "dev" 
+  if [ "$__mode" == "dev" ]; then
 
-(
-  exec "${TMPDIR}macos-setup/install.sh"
-)
+    # If STR="/path/to/foo.cpp", then
+    # ${STR##*/} returns foo.cpp (basepath)
+    if [ "${__script_dir##*/}" == "macos-setup" ]; then
+      # Copy current directory to work dir
+      cp -R . "${WORK_DIR}"
+      cd "${WORK_DIR}"
+      # Switch to "dev" branch
+      git checkout dev
+    else
+      error "Can't run in DEV mode outside of macos-setup directory!"
+      return 1
+    fi
 
-# Perform final cleanup
-echo -e "\033[1;32m[NOTICE  ] Removing macos-setup from the temporary location...\033[0m"
-rm -rf "${TMPDIR}macos-setup"
+  else
+
+    # Clone the repository from master the normal way
+    git clone https://github.com/vduseev/macos-setup.git "${WORK_DIR}"
+    cd "${WORK_DIR}"
+
+  fi
+
+  return 0
+}
+
+# Make sure that the temporary directory to which the files have been
+# copied is always cleaned up after work is done.
+cleanup_tmp_dir() {
+  notice "Removing macos-setup from the temporary location ..."
+  rm -rf "${WORK_DIR}"
+}
+
+install() {
+   ensure_pip
+   ensure_ansible
+
+   return 1
+}
+
+ensure_pip() {
+   notice "Making sure pip is installed ..."
+   if hash pip 2>/dev/null; then
+     info "pip is already installed"
+     printf "$(pip --version)\n"
+   else
+     info "pip not found. Installing pip ..."
+     sudo easy_install pip
+   fi
+}
+
+ensure_ansible() {
+   notice "Making sure ansible is installed ..."
+   if hash ansible 2>/dev/null; then
+     info "ansible is already installed"
+     printf "$(ansible --version)\n"
+   else
+     info "ansible not found. Installing ansible ..."
+     sudo -H pip install ansible
+   fi
+}
+
+# Run main function
+main $1
